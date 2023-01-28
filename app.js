f92146fb4ed013392b0cdfdc244a2956436b1a35
CookieStudioCanada <92235638+CookieStudioCanada@users.noreply.github.com>
2023-01-28 12:16:49 -0500

    Beta - V0

---
+++ app.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementById('toggle-button');
+const dropdown = document.getElementById('dropdown');
+
+dropdown.style.display = 'none'
+
+toggleButton.addEventListener('click', () => {
+
+  // Add transition...
+  dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
+});
+
+const navLinks = document.querySelector('.navlinks');
+
+navLinks.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    navLinks.classList.remove('open');
+    const sectionId = event.target.getAttribute('href');
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+});
