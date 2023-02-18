dbf4c0dd44cb748a311ffd3a9c65141b316199cd
CookieStudioCanada <92235638+CookieStudioCanada@users.noreply.github.com>
2023-02-18 18:32:07 -0500

    Reviews

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Hamburger menu
+
 const toggleButton = document.getElementById('toggle-button');
 const dropdown = document.getElementById('dropdown');
 
@@ -18,4 +20,30 @@ navLinks.addEventListener('click', (event) => {
     const section = document.querySelector(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+// Reviews
+
+const reviews = ["This is the first review", "This is the second review", "This is the third review"]; // Objects : nom, commentaire, rating
+
+const reviewsContent = document.querySelector(".reviews-content");
+const leftArrow = document.querySelector(".reviews-left-arrow");
+const rightArrow = document.querySelector(".reviews-right-arrow");
+
+let reviewIndex = 0;
+
+function displayReview() {
+  reviewsContent.textContent = reviews[reviewIndex];
+}
+
+displayReview();
+
+leftArrow.addEventListener("click", () => {
+  reviewIndex = (reviewIndex === 0) ? reviews.length - 1 : reviewIndex - 1;
+  displayReview();
+});
+
+rightArrow.addEventListener("click", () => {
+  reviewIndex = (reviewIndex === reviews.length - 1) ? 0 : reviewIndex + 1;
+  displayReview();
 });
